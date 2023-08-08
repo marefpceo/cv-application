19a498c474f3a4c2eb1a7832bd55189c2b326219
marefpceo <32721274+marefpceo@users.noreply.github.com>
2023-08-07 17:04:03 -0700

    Create tabs for InputSection
    Three tabs created, one for each section. Applied styling and state
    handling to toggle between the 3 tabs to show which is current.
    
    Icons color was changed for better visualization.

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -1,10 +1,47 @@
+import { useState } from 'react'
+import infoIconLight from '../assets/info-icon-light.png'
+import schoolIconLight from '../assets/school-icon-light.png'
+import workIconLight from '../assets/work-history-icon-light.png'
 import '../styles/InputSection.css'
 
 function InputSection() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
-    <div className='input-section'>
-      
-    </div>
+    <section className='input-section'>
+      <nav id="input-nav">
+        <InputNavButtons 
+          icon={infoIconLight} 
+          isActive={activeIndex === 0}
+          onShow={() => setActiveIndex(0)}
+        />
+        <InputNavButtons 
+          icon={schoolIconLight} 
+          isActive={activeIndex === 1}
+          onShow={() => setActiveIndex(1)}
+        />
+        <InputNavButtons 
+          icon={workIconLight} 
+          isActive={activeIndex === 2}
+          onShow={() => setActiveIndex(2)}
+        />
+      </nav>
+
+      <div className="cards">
+
+      </div>
+    </section>
+  )
+}
+
+function InputNavButtons({ icon, isActive, onShow }) {
+
+  return (
+    <img src={icon} 
+      alt="General information input" 
+      onClick={onShow}
+      className={`input-icons ${isActive ? 'current' : ''}`}
+    />
   )
 }
 
